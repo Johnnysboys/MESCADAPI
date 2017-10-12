08402faebecd6cf8dbb86565bc72cca899975c64
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-10-12 10:54:56 +0200

    chaged method name to alertDiscarded

--- src/mes/IMESServer.java
+++ src/mes/IMESServer.java
@@ -7,8 +7,31 @@ import java.rmi.RemoteException;
 
 
 public interface IMESServer extends Remote{
+    /**
+     * Connecting party must register as an observer
+     * @param scada
+     * @throws RemoteException
+     */
     void addObserver(ISCADAObserver scada) throws RemoteException;
+
+    /**
+     * Alert to MES that order has been planted.
+     * @param o
+     * @throws RemoteException
+     */
     void alertPlanted(OrderINFO o) throws RemoteException;
-    void alertLost(OrderINFO o,int amountLost ) throws RemoteException;
+
+    /**
+     * Alert discarded always signals that a single quantity has been lost in production.
+     * @param o
+     * @throws RemoteException
+     */
+    void alertDiscarded(OrderINFO o) throws RemoteException;
+
+    /**
+     * Alert harvest always signals that a single quantity has ben harvested.
+     * @param o
+     * @throws RemoteException
+     */
     void alertHarvest(OrderINFO o) throws RemoteException;
 }

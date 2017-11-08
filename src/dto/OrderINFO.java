4bdc1b53c1803861bc778d1b254cfdacf0a09d7f
Mathias Tværmose Gleerup <mathiastgleerup@gmail.com>
2017-11-08 11:10:49 +0100

    fixed to articlenumber is string

--- src/dto/OrderINFO.java
+++ src/dto/OrderINFO.java
@@ -3,22 +3,22 @@ package dto;
 import java.io.Serializable;
 
 public class OrderINFO implements Serializable {
-    private int articleNumber;
+    private String articleNumber;
     private int quantity;
     private String orderID;
 
-    public OrderINFO(int articleNumber, int quantity, String orderID){
+    public OrderINFO(String articleNumber, int quantity, String orderID){
         this.articleNumber=articleNumber;
         this.quantity=quantity;
         this.orderID=orderID;
     }
 
 
-    public int getArticleNumber() {
+    public String getArticleNumber() {
         return articleNumber;
     }
 
-    public void setArticleNumber(int articleNumber) {
+    public void setArticleNumber(String articleNumber) {
         this.articleNumber = articleNumber;
     }
 
